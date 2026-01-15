92de8f6e51ad2467aabcb85a16ba9dc80e90ab67
lynn <okolo.ebelelynda@gmail.com>
2026-01-15 18:55:49 +0100

    contract address updated

--- frontend/src/components/marketplace/NFTCard.tsx
+++ frontend/src/components/marketplace/NFTCard.tsx
@@ -287,7 +287,7 @@ export function NFTCard({ tokenId, badgeName, badgeImage, ownerAddress, isOwnedB
 
       await waitForReceipt({
         client,
-        chain: MantleSepolia Testnet,
+        chain: MantleSepolia,
         transactionHash,
       });
 
@@ -354,7 +354,7 @@ export function NFTCard({ tokenId, badgeName, badgeImage, ownerAddress, isOwnedB
 
       await waitForReceipt({
         client,
-        chain: MantleSepolia Testnet,
+        chain: MantleSepolia ,
         transactionHash,
       });
 
