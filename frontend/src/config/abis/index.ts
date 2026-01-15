0ac41a4fb0852c2f9f4537a5db6dadc1ad23f47c
lynn <okolo.ebelelynda@gmail.com>
2026-01-15 18:26:10 +0100

    fix

--- frontend/src/config/abis/index.ts
+++ frontend/src/config/abis/index.ts
@@ -2,3 +2,4 @@ export { Mantle_Run_ABI } from './MantleRunnerABI';
 export { QUEST_TOKEN_ABI } from './QuestTokenABI';
 export { RUNNER_BADGE_ABI } from './RunnerBadgeABI';
 export { NFT_MARKETPLACE_ABI } from './NFTMarketplaceABI';
+export { STABLE_TOKEN_ABI } from './stableToken';
