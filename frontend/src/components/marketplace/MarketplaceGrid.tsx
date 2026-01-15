4b75e25b3cebf5ceec51bfdcd7b2ac02ca1fc9a0
lynn <okolo.ebelelynda@gmail.com>
2026-01-15 18:37:44 +0100

    fix mantle naming convention

--- frontend/src/components/marketplace/MarketplaceGrid.tsx
+++ frontend/src/components/marketplace/MarketplaceGrid.tsx
@@ -10,7 +10,7 @@ import { RUNNER_BADGE_ABI } from '@/config/abis';
 import { getBadgeImage, getBadgeNameByStage } from '@/config/badgeMetadata';
 import { NFTCard } from './NFTCard';
 
-const MantleSepolia Testnet = defineChain({
+const MantleSepolia = defineChain({
   id: 5003,
   name: "Mantle Sepolia Testnet",
   rpc: "https://rpc.sepolia.mantle.xyz/",
